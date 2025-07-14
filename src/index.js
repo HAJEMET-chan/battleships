97fee40eded77c179760df548d08c765fc4de152
HAJEMET-chan <157144868+HAJEMET-chan@users.noreply.github.com>
2025-07-14 16:14:37 +0300

    пкуа

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// src/index.js (or similar)
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Your Tailwind CSS import
-import App from '../gui'; // <--- Изменено здесь
+// import './index.css'; // Удалите эту строку, если index.css был только для Tailwind
+
+import App from '../gui'; // Импорт App из вашей новой папки gui
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
